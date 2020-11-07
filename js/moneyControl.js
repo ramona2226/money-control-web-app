ead8ddbbba45cd77a1d3e16ca8d1ce8a2e7569c8
Ramona2226 <brisc.ramona22@yahoo.com>
2020-11-07 13:06:08 +0200

    add transactions table

--- js/moneyControl.js
+++ js/moneyControl.js
@@ -2,9 +2,9 @@ window.MoneyControl= {
 
     API_URL: "http://localhost:8085",
 
-    createUser: function () {
-const existingUserFirstName = $("#user-first-name").val();
-    const existingUserSecondName = $("#user-second-name").val();
+    createUserOrReadUser: function () {
+        const existingUserFirstName = $("#user-first-name").val();
+        const existingUserSecondName = $("#user-second-name").val();
 
         let body = {
             firstName: "existingUserFirstName",
@@ -12,20 +12,32 @@ const existingUserFirstName = $("#user-first-name").val();
         }
         //nu imi arata url in (url{)
         $.ajax({
-            url:MoneyControl.API_URL + "/users",
+            url: MoneyControl.API_URL + "/users",
             method: "POST",
-            contentType:"application/json",
+            contentType: "application/json",
             data: JSON.stringify(body)
-         }).done(
-             // de vazut
-             function (response) {
-             console.log(response);
-            
+        }).done(
+            // de vazut
+            function (response) {
+                console.log(response);
+            })
+    },
+    bindEvents: function () {
+        $("#create-user").submit(function (event) {
+            event.preventDefault
+
+            MoneyControl.createUser();
+
         })
-            // mai pot avea alt comportament
-        //console.log("do something");
+
+        $("#login-user").submit(function (event) {
+            event.preventDefault
+// aici nu stiu cum
+            MoneyControl.getBalance(userId);
+
+        })
 
     }
-};
+}
 
-MoneyControl.createUser();
+
